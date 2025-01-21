438026ea856cc7179e4169945f62a52fa2643809
sudomaster00081 <a.jayprasadd0008@gmail.com>
2025-01-21 17:35:06 +0530

    deploy

---
+++ wgi.py
@@ -0,0 +1,6 @@
+# wsgi.py
+
+from app import app  # Import your Flask app instance
+
+if __name__ == "__main__":
+    app.run()  # This is optional and only needed if you want to run the app directly
